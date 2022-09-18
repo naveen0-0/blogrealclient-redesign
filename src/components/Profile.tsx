a005a169954104a6883329aa70f3094463257072
naveen0-0 <baddigamnaveenkumarreddy1910@gmail.com>
2022-09-18 23:14:08 +0530

    Added Profile Blogs

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,28 +1,53 @@
-
 import { Navigate } from "react-router-dom"
 import { useAuth } from "../hooks/useAuth"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { SERVER_BASE_URL } from "../constants"
+import Spinner from "./Spinner"
+import ProfileBlogs from "./ProfileBlogs"
 
 export default function Profile() {
   const { loggedIn, username, email } = useAuth()
-
-  if(!loggedIn) return <Navigate to="/"/>
+  const [loading, setLoading] = useState(true)
+  const [blogs, setBlogs] = useState([])
+  const [error, setError] = useState(false)
   
+  useEffect(() => {
+    getOwnBlogs()
+  },[])
+
+  const getOwnBlogs = async () => {
+    setLoading(true)
+    try {
+      let { data } = await axios.get(`${SERVER_BASE_URL}/api/ownblogs`,{ headers : { Authorization : localStorage.getItem('blogrealauthtoken')!}})
+      if(data.statusload){
+        setBlogs(data.blogs)
+      }else{
+        setError(true)
+      }
+    } catch (error) {
+      throw new Error("Axios Error Happened");
+    }finally{
+      setLoading(false)
+    }
+  }
+  
+  if(!loggedIn) return <Navigate to="/"/>
+  if(error) return <div className="text-center font-anton text-4xl md:text-6xl xl:text-[100px] flex justify-center items-center h-full drop-shadow-3xl">Error</div>
+
   return (
     <div className="w-[95%] md:w-[80%] mx-auto">
 
-      <div className="bg-slate-600 p-2 my-3 rounded-md shadow-2xl font-roboto flex flex-col md:flex-row">
-        <div className="bg-slate-900 text-slate-500 flex justify-center items-center min-w-[80px] mx-auto aspect-square rounded-full">{username[0].toUpperCase()}</div>
+      <div className="bg-slate-900 p-2 my-3 rounded-md shadow-2xl font-roboto flex flex-col md:flex-row items-center">
+        <div className="bg-slate-500 text-slate-900 flex justify-center items-center w-[80px] h-[80px] mx-auto rounded-full">{username[0].toUpperCase()}</div>
 
-        <div className="flex-1 my-2 text-center">
-          <div className="p-1">{username}</div>
-          <div className="p-1">{email}</div>
+        <div className="flex-1 my-2 text-center text-xl text-slate-400">
+          <div>{username}</div>
+          <div>{email}</div>
         </div>
       </div>
 
-
-      <div className="bg-slate-600 p-2 my-3 rounded-md shadow-2xl font-roboto">
-        Own Blogs
-      </div>
+      {loading? <Spinner/> : <ProfileBlogs blogs={blogs}/> }
 
     </div>
   )
---
+++ src/components/ProfileBlogs.tsx
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom"
+import { TbEdit } from 'react-icons/tb'
+import { MdDelete } from 'react-icons/md'
+import { BiLinkExternal } from 'react-icons/bi'
+
+type BlogType = {
+  url : string,
+  title : string,
+  description : string,
+  _id:string
+}
+
+
+export default function ProfileBlogs({ blogs }:any) {
+  return (
+    <div>
+      <div className="font-anton text-3xl text-center">Your Blogs</div>
+      {blogs.map((blog:BlogType) => <Blog blog={blog} key={blog._id}/>)}
+    </div>
+  )
+}
+
+
+const Blog = ({ blog }:{ blog:BlogType }) => {
+
+  const editBlog = () => {
+    console.log("Blog Edited")
+  }
+
+  const deleteBlog = () => {
+    console.log("Blog Deleted")
+  }
+
+
+  return (
+      <div className="bg-slate-900 my-3 rounded-md shadow-2xl font-roboto">
+        <div>
+          <img src={blog.url} alt={blog.title} className="w-full h-[100px] object-cover rounded-t-md"/>
+        </div>
+
+        <div className="px-2">
+          <div className="my-2 text-2xl truncate text-slate-400">{blog.title}</div>
+          <div className="my-2 text-md truncate text-slate-500">{blog.description}</div>
+        </div>
+
+        <div className="p-2 flex">
+          <Link to={`/${blog._id}`}>
+            <button title="Go to Blog" className="hover:bg-slate-800 rounded-full p-2"><BiLinkExternal size={20} color="#64748B"/></button>
+          </Link>
+          <button title="Edit" onClick={editBlog} className="ml-4 hover:bg-slate-800 rounded-full p-2"><TbEdit size={22} color="#64748B"/></button>
+          <button title="Delete" onClick={deleteBlog} className="ml-4 hover:bg-slate-800 rounded-full p-2"><MdDelete size={22} color="#64748B"/></button>
+        </div>
+      </div>
+  )
+}
