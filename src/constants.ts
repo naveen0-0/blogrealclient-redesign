b61297f96b53255f22d56dddbbb73299159139d5
naveen0-0 <baddigamnaveenkumarreddy1910@gmail.com>
2022-09-20 16:53:23 +0530

    Server url changed from heroku to railway

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
-export const SERVER_BASE_URL = "https://blogreal.herokuapp.com"
+// export const SERVER_BASE_URL = "https://blogreal.herokuapp.com"
+export const SERVER_BASE_URL = "https://realblogserver-production.up.railway.app"
