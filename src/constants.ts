f745ef2afa3be841416481f6f759e15c4232891f
naveen0-0 <baddigamnaveenkumarreddy1910@gmail.com>
2022-09-20 16:58:18 +0530

    fd

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-// export const SERVER_BASE_URL = "https://blogreal.herokuapp.com"
+// export const SERVER_BASE_URL = "http://localhost:5000"
 export const SERVER_BASE_URL = "https://realblogserver-production.up.railway.app"
